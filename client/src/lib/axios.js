99436982e2c14938c3c39c332ca647e9896d7825
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-06-29 17:50:37 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL:'http://localhost:8000/api',
+    baseURL:'https://auth-system-server-uhow.onrender.com/api',
     withCredentials:true
 });
